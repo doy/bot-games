a6d26493678ce9ff61dcf96d735f809340104f81
doy <doy@tozt.net>
2009-01-24 01:55:28 -0500

    i really want to be reblessing, not completely recreating the method metaclass

--- lib/Bot/Games/Meta/Role/Class.pm
+++ lib/Bot/Games/Meta/Role/Class.pm
@@ -6,14 +6,17 @@ after ((map { "add_${_}_method_modifier" } qw/before after around/) => sub {
     my $self = shift;
     my $name = shift;
 
-    my $method_meta = $self->remove_method($name);
+    my $method_meta = $self->get_method($name);
+    my $orig_method_meta = $method_meta->get_original_method;
+    return unless $orig_method_meta->meta->can('does_role')
+               && $orig_method_meta->meta->does_role('Bot::Games::Meta::Role::Command');
+    my $pass_args = $orig_method_meta->pass_args;
     my $method_metaclass = Moose::Meta::Class->create_anon_class(
         superclasses => [blessed $method_meta],
         roles        => ['Bot::Games::Meta::Role::Command'],
         cache        => 1,
-    )->name;
-    $method_meta = $method_metaclass->wrap($method_meta);
-    $self->add_method($name, $method_meta);
+    );
+    $method_metaclass->rebless_instance($method_meta, pass_args => $pass_args);
 });
 
 no Moose::Role;
