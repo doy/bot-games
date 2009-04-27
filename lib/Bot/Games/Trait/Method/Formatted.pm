4671d44db2cd128f64cd0607c0fce2daaa291880
doy <doy@tozt.net>
2009-04-26 22:54:01 -0500

    don't include a default formatter, since it will (probably) never be used

--- lib/Bot/Games/Trait/Method/Formatted.pm
+++ lib/Bot/Games/Trait/Method/Formatted.pm
@@ -5,18 +5,8 @@ has formatter => (
     is      => 'rw',
     isa     => 'CodeRef',
     default => sub { sub {
-        my $self = shift;
-        my ($to_print) = @_;
-        if (blessed $to_print) {
-            $to_print = "$to_print";
-        }
-        elsif (ref($to_print) && ref($to_print) eq 'ARRAY') {
-            $to_print = join ', ', @$to_print;
-        }
-        elsif (!$to_print) {
-            $to_print = 'false';
-        }
-        return $to_print;
+        warn "no formatter specified!";
+        return @_;
     } },
 );
 
