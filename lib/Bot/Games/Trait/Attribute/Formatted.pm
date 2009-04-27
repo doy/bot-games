32fd1dc2b00e47196e1eba9c394c37a6957558ae
doy <doy@tozt.net>
2009-04-26 22:45:31 -0500

    fix up the formatter attribute metaclass to be more like the command attribute metaclass, so that it actually works, hopefully

--- lib/Bot/Games/Trait/Attribute/Formatted.pm
+++ lib/Bot/Games/Trait/Attribute/Formatted.pm
@@ -10,13 +10,31 @@ has formatter => (
     predicate => 'has_formatter',
 );
 
+before _process_options => sub {
+    my $self = shift;
+    my ($name, $options) = @_;
+    warn "only commands will have a formatter applied"
+        if exists($options->{formatter}) && !$options->{command};
+};
+
+around accessor_metaclass => sub {
+    my $orig = shift;
+    my $self = shift;
+    my $metaclass = $self->$orig(@_);
+    return $metaclass unless $self->has_formatter;
+    return Moose::Meta::Class->create_anon_class(
+        superclasses => [$metaclass],
+        roles        => ['Bot::Games::Trait::Method::Formatted'],
+        cache        => 1,
+    )->name;
+};
+
 after install_accessors => sub {
     my $self = shift;
     if ($self->has_formatter) {
         my $formatter = $self->formatter;
         my $method_meta = $self->get_read_method_ref;
-        $method_meta->formatter($formatter)
-            if $method_meta->can('formatter');
+        $method_meta->formatter($formatter);
     }
 };
 
