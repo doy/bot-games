433b5b5c12fa697e1a6ea2b1d34a1ba3dd9d009d
Jesse Luehrs <doy@tozt.net>
2009-10-31 15:42:50 -0500

    have Bot::Games::OO set the base class

--- lib/Bot/Games/OO.pm
+++ lib/Bot/Games/OO.pm
@@ -2,8 +2,18 @@ package Bot::Games::OO;
 use IM::Engine::Plugin::Commands::OO ();
 use Moose::Exporter;
 
-Moose::Exporter->setup_import_methods(
-    also => ['IM::Engine::Plugin::Commands::OO'],
+my ($import, $unimport, $init_meta) = Moose::Exporter->build_import_methods(
+    install => [qw(import unimport)],
+    also    => ['IM::Engine::Plugin::Commands::OO'],
 );
+
+sub init_meta {
+    my ($package, %options) = @_;
+    $options{base_class} = 'Bot::Games::Game'
+        if !exists $options{base_class}
+        && $options{for_class} ne 'Bot::Games::Game';
+    IM::Engine::Plugin::Commands::OO->init_meta(%options);
+    goto $init_meta;
+}
 
 1;
